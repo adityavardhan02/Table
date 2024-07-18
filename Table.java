e396fe1f0c3e3ad8e2eb589dfbee42f88ee478cd
Aditya vardhan <adityadannapaneni@gmail.com>
2024-07-18 20:24:20 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,16 @@
+package apjfsa;
+
+public class Table {
+	static synchronized void printTable(int num) {
+		for(int i=1;i<=10;i++) {
+			
+		}
+			
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
